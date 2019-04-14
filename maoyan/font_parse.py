537ae3fe2428978e195ffd55c96788f7ef7a7943
zhangsen <358974225@qq.com>
2019-04-14 16:47:14 +0800

    字体解析

--- maoyan/font_parse.py
+++ maoyan/font_parse.py
@@ -1,7 +1,7 @@
 from fontTools.ttLib import TTFont
 import os
 a ={}
-font=TTFont('./maoyan.woff')#读取woff文件
+font=TTFont('./fonts/font.woff')#读取woff文件
 # font.saveXML('./m.xml')#转成xml
 
 
@@ -9,16 +9,18 @@ def modify_data(data):
     # 获取 GlyphOrder 节点
     gly_list = font.getGlyphOrder()
     # 前两个不是需要的值，截掉
+    print(gly_list)
     gly_list = gly_list[2:]
     # 枚举, number是下标，正好对应真实的数字，gly是乱码
     for number, gly in enumerate(gly_list):
         # 把 gly 改成网页中的格式
-        gly = gly.replace('uni', '\\u').lower()
+        gly = gly.replace('uni', '').lower()
+        print(gly)
         # 如果 gly 在字符串中，用对应数字替换
-        data = data.replace(gly, str(number))
+        data = data.replace('&#x'+gly+';', str(number))
     # 返回替换后的字符串
     return data
-a = modify_data(r'\uf361.\uf1a8')
+a = modify_data(r'&#xf2e6;.&#xf412;')
 print(a)
 # num = [6, 2, 9, 0, 1, 8, 4, 3, 7, 5]
 # list=font.getGlyphOrder()[2:]
--- maoyan/movie_list.py
+++ maoyan/movie_list.py
@@ -18,31 +18,49 @@ def get_movie_info(url):
     url = 'http:' + re.search('url\(\'(//vfile.meituan.net/colorstone/.*\.woff)', font_face)[1]
     name = re.search('/colorstone/(.*.woff)\'\) format', font_face)[1]
     get_font(url, name)
-    font = TTFont('./fonts/font.woff')
+    font = TTFont('./fonts/test.woff')
     font.saveXML('./m.xml')  # 转成xml
-    xml = ET.parse('./m.xml')
-    tags = xml.findall('.//hmtx/mtx')
-    t_list = []
-    for tag in tags:
-        t_list.append(tag.attrib['name'])
-    a = get_num(t_list, font_str)
+    print(font_str)
+    # xml = ET.parse('./m.xml')
+    # tags = xml.findall('.//hmtx/mtx')
+    # t_list = []
+    # for tag in tags:
+    #     t_list.append(tag.attrib['name'])
+    temp, pat = get_num()
+    response_index = re.sub(pat, lambda x: temp[x.group()], font_str)
+    print(response_index)
+    #
+    # print(a)
 
-    print(a)
+def get_num():
+    baseFonts = TTFont('./fonts/font.woff')
+    base_fonts = ['uniF27B', 'uniE296', 'uniE732', 'uniEFDF',
+                 'uniF3AB', 'uniF8EC', 'uniEF37', 'uniE7EE', 'uniE5C5', 'uniEBD9']
+    base_nums = ['9','5', '1', '8', '3','2','0','3','4','7','6']
+    online_fonts = TTFont('./fonts/test.woff')
+    uni_list = online_fonts.getGlyphNames()[1:-1]
+    temp = {}
+    for i in range(10):
+        onlineGlyph = online_fonts['glyf'][uni_list[i]]
+        for j in range(10):
+            baseGlyph = baseFonts['glyf'][base_fonts[j]]
+            if onlineGlyph == baseGlyph:
+                temp["&#x" + uni_list[i][3:].lower() + ';'] = base_nums[j]
+    pat = '(' + '|'.join(temp.keys()) + ')'
+    return temp, pat
 
-def get_num(num_list, data):
-    print(data, num_list)
-    for number, gly in enumerate(num_list):
-        # 把 gly 改成网页中的格式
-        gly = gly[-4:]
-        gly =  '&#x' + gly.lower() + ';'
-        print(gly)
-        # 如果 gly 在字符串中，用对应数字替换
-        data = data.replace(gly, str(number))
-    return data
+    # for number, gly in enumerate(num_list):
+    #     # 把 gly 改成网页中的格式
+    #     gly = gly[-4:]
+    #     gly =  '&#x' + gly.lower() + ';'
+    #     print(gly)
+    #     # 如果 gly 在字符串中，用对应数字替换
+    #     data = data.replace(gly, str(number))
+    # return data
 
 def get_font(url, name):
     res = requests.get(url)
-    with open('./fonts/font.woff', 'wb') as f:
+    with open('./fonts/test.woff', 'wb') as f:
         f.write(res.content)
 
 if __name__ == '__main__':
