821304306d72c462ba2eb09f9be05ccba6c102d4
senzhang <358974225@qq.com>
2019-07-29 14:11:38 +0800

    add get_proxy

--- tools/tools.py
+++ tools/tools.py
@@ -5,6 +5,7 @@ import re
 import requests
 import os
 
+
 def data_to_json(data, name):
     with io.open(name + '.json', 'w', encoding='utf-8') as fo:
         fo.write(json.dumps(data, ensure_ascii=False, indent=2, separators=(',', ': ')))
@@ -45,6 +46,7 @@ def parse_price(num_str):
         print(e)
         return num_str
 
+
 def get_img(url, path, name=None):
     if not name:
         name = url.split('/')[-1]
@@ -55,3 +57,31 @@ def get_img(url, path, name=None):
     return path
 
 
+class GetProxy(object):
+    def __init__(self):
+        self.proxy_url = 'http://api.ip.data5u.com/dynamic/get.html?order=c77bcb61b876d0efe082d749462f13a7&json=1&sep=3'
+
+    def get_proxy(self):
+        res = requests.get(self.proxy_url)
+        proxy_data = json.loads(res.text)
+        if proxy_data['msg'] == 'ok':
+            proxy_dict = proxy_data['data'][0]
+            proxy = proxy_dict['ip'] + ':' + str(proxy_dict['port'])
+            proxies = {
+                'http': 'http://' + proxy,
+                'https': 'https://' + proxy
+            }
+            res = requests.get('https://www.baidu.com', proxies=proxies)
+            print(res.status_code)
+            if res.status_code == 200:
+                return proxies
+
+
+def get_proxy():
+    proxy = GetProxy()
+    return proxy.get_proxy()
+
+
+if __name__ == '__main__':
+    proxy = get_proxy()
+    print(proxy)
